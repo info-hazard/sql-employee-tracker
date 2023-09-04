9b7a42569ca9241de72db9956b84062816c90cd6
info-hazard <107716504+info-hazard@users.noreply.github.com>
2023-09-04 01:51:14 -0700

    added sql connection variable and created initial questions for inquirer prompt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // importing npm packages
 const inquirer = require("inquirer");
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 
 // encryption for db access
 require("dotenv").config();
@@ -9,3 +9,29 @@ require("dotenv").config();
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 const dbName = process.env.DB_NAME;
+
+// creating sql connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: dbUser,
+  password: dbPassword,
+  database: dbName,
+});
+
+const questions = [
+  {
+    type: 'list',
+    name: 'questions',
+    message: 'What would you like to do?',
+    choices: [
+      'View All Employees',
+      'View All Roles',
+      'View All Departments',
+      'Add Employee',
+      'Add Role',
+      'Add Department',
+      'Update Role for Employee',
+      'QUIT',
+    ],
+  },
+];
